2082b796db776c867859fae4434c544c20035f86
RanDReille <58237825+RanDReille@users.noreply.github.com>
2019-12-01 17:17:09 +0900

    Add files via upload

--- Scenes/UI/Scripts/UI.gd
+++ Scenes/UI/Scripts/UI.gd
@@ -5,6 +5,13 @@ func _ready():
 
 func _physics_process(delta):
 	$Power.value = GLOBAL_SCRIPTS.player_power
+	if Input.is_action_just_pressed("ui_cancel"):
+		var is_paused = get_tree().paused
+		get_tree().paused = !is_paused
+		if is_paused:
+			$Pause_Rect.hide()
+		else:
+			$Pause_Rect.show()
 
 func _on_Player_life_change(life_val):
 	$Life.value = life_val
